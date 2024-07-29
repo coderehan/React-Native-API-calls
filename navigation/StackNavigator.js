13214eb3655e624975bda8ce9c75de50e9da4091
coderehan <mohammedrehan6532@gmail.com>
2024-07-29 12:04:38 +0530

    Splash Screen added

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -3,15 +3,17 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import SignupScreen from "../screens/SignUpScreen";
 import LoginScreen from "../screens/LoginScreen";
-import { BottomTabNavigator } from "./BottomTabNavigator"; 
+import { BottomTabNavigator } from "./BottomTabNavigator";
 import PasswordUpdateScreen from "../screens/PasswordUpdateScreen";
+import SplashScreen from "../screens/SplashScreen";
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
-  
+
   return (
-    <Stack.Navigator initialRouteName={'Login Screen'}>
+    <Stack.Navigator initialRouteName={'Splash Screen'}>
+      <Stack.Screen name="Splash Screen" component={SplashScreen} options={{ headerShown: false }} /> 
       <Stack.Screen name="Login Screen" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignUp Screen" component={SignupScreen} />
       <Stack.Screen name="Password Update Screen" component={PasswordUpdateScreen} />
---
+++ screens/SplashScreen.js
@@ -0,0 +1,44 @@
+// SplashScreen.js
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const SplashScreen = ({ navigation }) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            // Navigate to the desired screen after 3 seconds
+            navigation.replace('Login Screen'); // or any initial screen you prefer
+        }, 3000);
+
+        return () => clearTimeout(timer); // Cleanup the timer on component unmount
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/images/image_reactnative.png')} 
+                style={styles.image}
+            />
+            <Text style={styles.text}>Welcome to React Native App!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+    },
+    image: {
+        width: 150, 
+        height: 150, 
+        marginBottom: 20, 
+    },
+    text: {
+        fontSize: 24,
+        color: 'black',
+    },
+});
+
+export default SplashScreen;
